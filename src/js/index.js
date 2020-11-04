a77bdc653ae4a4d923b49b3830654f3cda632788
Virus <vladprodan6@gmail.com>
2020-11-04 02:06:30 +0200

    promo

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
 import 'bootstrap';
-
 import '../scss/index.scss';
 
 $('#alert').on('click', () => {
   alert('jQuery works!');
 });
-
 // Your jQuery code
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
